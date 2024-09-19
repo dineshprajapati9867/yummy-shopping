87c2a4a12e2b7ff74414f7a79666fd9540397e74
Dinesh Prajapati <144621296+dineshprajapati9867@users.noreply.github.com>
2024-09-19 15:34:23 +0530

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -33,7 +33,7 @@ const Register = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/register", {
+      const response = await fetch("https://yummy-shopping-server.onrender.com/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
